d898b3b97bc43be343f5e802fb4289b36a32a81f
andrewfowl <56274845+andrewfowl@users.noreply.github.com>
2023-03-03 11:40:21 -0500

    reverse particles

--- sketch.js
+++ sketch.js
@@ -1,20 +1,10 @@
 let r;
 let factor = 0;
-let particles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 360, 100, 100, 1);
   r = height / 2 - 16;
-
-  // Create a grid of particles within the circle shape
-  for (let x = -r; x <= r; x += 4) {
-    for (let y = -r; y <= r; y += 4) {
-      if (dist(x, y, 0, 0) < r) {
-        particles.push(new Particle(x, y));
-      }
-    }
-  }
 }
 
 function getVector(index, total) {
@@ -26,15 +16,13 @@ function getVector(index, total) {
 
 function draw() {
   background(0);
-  const total = 20;
+  const total = 200;
   factor += 0.015;
 
   translate(width / 2, height / 2);
-
-  // Draw the sand particles
-  particles.forEach(particle => {
-    particle.draw();
-  });
+  strokeWeight(1);
+  noFill();
+  ellipse(0, 0, r * 2);
 
   for (let i = 0; i < total; i++) {
     const a = getVector(i, total);
@@ -46,44 +34,16 @@ function draw() {
     const pointColor = lerpColor(colorFrom, colorTo, colorAmount);
     stroke(pointColor);
 
-    // Draw a Bezier curve between the sand particles
-    particles.forEach(particle => {
-      const d1 = dist(particle.pos.x, particle.pos.y, a.x, a.y);
-      const d2 = dist(particle.pos.x, particle.pos.y, -b.x, b.y + b.y);
-      if (d1 < r && d2 < r) {
-        particle.applyForce(createVector(-particle.pos.x, -particle.pos.y).normalize().mult(0.05));
-      }
-    });
-  }
-  
-  // Update and apply forces to the sand particles
-  particles.forEach(particle => {
-    particle.update();
-    particle.applyForce(createVector(0, 0.05));
-  });
-}
+    // Constrain the x and y coordinates of the Bezier curve points to the bounds of the circle
+    const x1 = constrain(a.x, -r, r);
+    const y1 = constrain(a.y, -r, r);
+    const x2 = constrain(a.x, -r, r);
+    const y2 = constrain(-a.y, -r, r);
+    const x3 = constrain(-b.x, -r, r);
+    const y3 = constrain(b.y + b.y, -r, r);
+    const x4 = constrain(-b.x, -r, r);
+    const y4 = constrain(-b.y, -r, r);
 
-// Particle class
-class Particle {
-  constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = createVector(0, 0);
-    this.acc = createVector(0, 0);
-  }
-
-  applyForce(force) {
-    this.acc.add(force);
-  }
-
-  update() {
-    this.vel.add(this.acc);
-    this.vel.limit(5);
-    this.pos.add(this.vel);
-    this.acc.set(0, 0);
-  }
-
-  draw() {
-    strokeWeight(1);
-    point(this.pos.x, this.pos.y);
+    bezier(x1, y1, x2, y2, x3, y3, x4, y4);
   }
 }
